7483b2daff8ef87b0ce0592b2ea05aa9bb437a18
adrian500502 <103137359+adrian500502@users.noreply.github.com>
2024-05-23 21:51:16 +0200

    Add displaying dynamically fetched job data

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  JOBS = load_jobs_from_db()
-  return render_template("home.html", jobs=JOBS, company_name="Company")
+def enter_home():
+  jobs = load_jobs_from_db()
+  return render_template("home.html", jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
-  JOBS = load_jobs_from_db()
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  fetched_job = load_job_from_db(id)
+
+  if not fetched_job:
+    return "Not Found", 404
+  
+  return render_template("jobpage.html", job=fetched_job)
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=8000)
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
-import os
+import os, sys
 
 DATABASE_CONNECTION_STRING = os.environ.get('DATABASE_CONNECTION_STRING')
 
@@ -14,3 +14,12 @@ def load_jobs_from_db():
       jobs.append(dict(row))
     
     return jobs
+  
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text(f"select * from jobs where id={id}"))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
